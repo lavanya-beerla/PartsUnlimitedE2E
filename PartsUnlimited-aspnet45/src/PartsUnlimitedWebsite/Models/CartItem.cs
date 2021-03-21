06ace9c7a76cb0a56641b015ec48178667adf6da
lavanya <beerla.lavanya@accenture.com>
2021-03-21 11:42:28 +0530

    my commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //my second change
+        //my first change
         [Key]
         public int CartItemId { get; set; }
 
